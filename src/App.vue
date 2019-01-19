3d2680f69312dcc1f6b3aa5ad18eb469a5f320eb
Sergey Bochkov <bochkovsa@gmail.com>
2019-01-19 17:03:11 +0500

    menu, gallery, background

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
-    <img alt="M17 logo" src="./assets/logo.jpg">
-    <Gigs/>
-    <Contacts/>
+    <el-container>
+      <el-header height="913px">
+      </el-header>
+      <el-main>
+        <Menu />
+        <img alt="M17 logo" src="./assets/logo.jpg">
+        <Gigs/>
+        <Contacts/>
+      </el-main>
+      <el-footer>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
+import Menu from "./components/Menu.vue";
 import Gigs from "./components/Gigs.vue";
 import Contacts from "./components/Contacts.vue";
+import Gallery from "./components/Gallery.vue";
 
 export default {
   name: "app",
   components: {
+    Menu,
     Gigs,
+    Gallery,
     Contacts
   }
 };
@@ -23,12 +36,24 @@ export default {
 html {
   background: #040507;
 }
+body {
+  padding: 0;
+  margin: 0;
+}
+.el-header {
+  background-image: url("./assets/header.jpg");
+  background-repeat: no-repeat;
+  background-position: center top;
+  background-size: cover;
+  display: block;
+}
 #app {
-  font-family: Impact, 'Oswald', Helvetica, Arial, sans-serif;
+  font-family: Impact, "Oswald", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: aliceblue;
-  margin-top: 60px;
+  margin: 0px;
+  padding: 0px;
 }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+<el-menu mode="horizontal" default-active="1" background-color="#040507" text-color="#FFFFFF" active-text-color="#FF0000">
+    <el-menu-item index="1">Новости</el-menu-item>
+    <el-menu-item index="2">Состав</el-menu-item>
+</el-menu>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.el-menu.el-menu--horizontal {
+    border-bottom: none;
+}
+.el-menu-item {
+    font-size: 1.5em;
+}
+</style>
