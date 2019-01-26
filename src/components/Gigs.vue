5ead5c92aa0c114395a523eaba734ec86a875e80
Sergey Bochkov <bochkovsa@gmail.com>
2019-01-26 12:04:24 +0500

    empty news

--- src/components/Gigs.vue
+++ src/components/Gigs.vue
@@ -1,27 +1,35 @@
 <template>
   <div class="gigs">
-    <img alt="M17 logo" src="@/assets/logo.jpg">
-    <h1>Ближайшие выступления</h1>
-    <ul v-for="(gig) in gigs" :key="gig.id">
-      <li>
-        <span class="dt">
-          {{gig.date|moment("LLLL")}}
-        </span>
-        <br>
-        <span class="gig.text">
-          {{gig.place.name}}<br/>{{gig.place.address}}
-        </span>
-      </li>
-    </ul>
+    <div v-if="isEmpty"></div>
+    <div v-else>
+      <img alt="M17 logo" src="@/assets/logo.jpg">
+      <h1>Ближайшие выступления</h1>
+      <ul v-for="(gig) in gigs" :key="gig.id">
+        <li>
+          <span class="dt">{{gig.date|moment("LLLL")}}</span>
+          <br>
+          <span class="gig.text">
+            {{gig.place.name}}
+            <br>
+            {{gig.place.address}}
+          </span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import {axio} from '../App.vue';
+import { axio } from "../App.vue";
 export default {
   name: "Gigs",
   created: function() {
-    axio.get("/api/v1/gigs").then((resp) => this.gigs = resp.data);
+    axio.get("/api/v1/gigs").then(resp => (this.gigs = resp.data));
+  },
+  computed: {
+    isEmpty: function() {
+      return this.gigs.length === 0;
+    }
   },
   data: function() {
     return {
