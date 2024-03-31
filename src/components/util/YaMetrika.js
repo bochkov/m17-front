2293be015fd2fdf9dad5bca052deebde0d6eec1e
Sergey Bochkov <bochkovsa@gmail.com>
2024-03-31 15:00:54 +0500

    upd metrika

--- src/components/util/YaMetrika.js
+++ src/components/util/YaMetrika.js
@@ -26,9 +26,11 @@ function YandexMetrikaTag({
 
   // Script that injects Yandex Metrika tag script.
   const scriptInjectorHTML = `
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments);};
-    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
   `;
 
   return (
@@ -78,7 +80,6 @@ export default function YandexMetrika({
   webvisor = true,
 }) {
   /// Yandex Metrika service.
-  const env = process.env.NODE_ENV
   return (
     <>
       <YandexMetrikaTag
